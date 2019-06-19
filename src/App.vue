2b2d25501196377d8c0d425e801f75b2602b853b
cxtao666 <1246784448@qq.com>
2019-06-19 22:21:18 +0800

    init my project

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template> 
+<div >
+    <div class="paddingtop"></div>
+    <mt-header fixed title="固定在顶部"></mt-header>
+    <h1>哈哈</h1>
+<nav class="mui-bar mui-bar-tab">
+  	<a class="mui-tab-item mui-active">
+  		<span class="mui-icon mui-icon-home"></span>
+  		<span class="mui-tab-label">首页</span>
+  	</a>
+  	<a class="mui-tab-item">
+  		<span class="mui-icon mui-icon-phone"></span>
+  		<span class="mui-tab-label">电话</span>
+  	</a>
+  	<a class="mui-tab-item">
+  		<span class="mui-icon mui-icon-email"></span>
+  		<span class="mui-tab-label">邮件</span>
+  	</a>
+  	<a class="mui-tab-item">
+  		<span class="mui-icon mui-icon-gear"></span>
+  		<span class="mui-tab-label">设置</span>
+  	</a>
+  </nav>
+ 
+
+
+
+
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.paddingtop{
+
+    padding-top: 44px;
+}
+</style>
