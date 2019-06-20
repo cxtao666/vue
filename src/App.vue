a7a7dfdc1f1dfa23d84bfc852aa2ac7c18ceef1e
cxtao666 <1246784448@qq.com>
2019-06-20 23:04:02 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,22 @@
     <mt-header fixed title="固定在顶部"></mt-header>
     <h1>哈哈</h1>
 <nav class="mui-bar mui-bar-tab">
-  	<a class="mui-tab-item mui-active">
+  	<router-link class="mui-tab-item " to="/home">
   		<span class="mui-icon mui-icon-home"></span>
   		<span class="mui-tab-label">首页</span>
-  	</a>
-  	<a class="mui-tab-item">
-  		<span class="mui-icon mui-icon-phone"></span>
-  		<span class="mui-tab-label">电话</span>
-  	</a>
-  	<a class="mui-tab-item">
-  		<span class="mui-icon mui-icon-email"></span>
-  		<span class="mui-tab-label">邮件</span>
-  	</a>
-  	<a class="mui-tab-item">
-  		<span class="mui-icon mui-icon-gear"></span>
-  		<span class="mui-tab-label">设置</span>
-  	</a>
+  	</router-link>
+  	<router-link class="mui-tab-item" to="/member" >
+  		<span class="mui-icon mui-icon-person"></span>
+  		<span class="mui-tab-label">会员</span>
+  	</router-link>
+  	<router-link class="mui-tab-item" to="/shopcar">
+  		<span class="mui-icon mui-icon-extra mui-icon-extra-cart"></span>
+  		<span class="mui-tab-label">购物车</span>
+  	</router-link>
+  	<router-link class="mui-tab-item" to="/search">
+  		<span class="mui-icon mui-icon-search"></span>
+  		<span class="mui-tab-label">搜索</span>
+  	</router-link>
   </nav>
  
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import app from './App.vue'
 
+import vuerouter from 'vue-router'
 
 import  './lib/mui/css/mui.min.css'
 import { Header } from 'mint-ui'
+
+Vue.use(vuerouter)
+import "./lib/mui/mui/css/icons-extra.css"
+
+
+
+import router from './router.js'
 
 Vue.component(Header.name,Header)
 
@@ -11,5 +19,6 @@ Vue.component(Header.name,Header)
 
 var vm = new Vue({
     el:"#app",
-   render:c => c(app)
+   render:c => c(app),
+   router
 })
